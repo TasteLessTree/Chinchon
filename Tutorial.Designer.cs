f6e73b569e2c6e8b258bb6e2b4d66de94987a5f9
Andrés <spelujo@gmail.com>
2025-07-04 10:29:15 +0200

    Añadido tutorial

--- Tutorial.Designer.cs
+++ Tutorial.Designer.cs
@@ -23,16 +23,18 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Tutorial));
             titulo = new Label();
             instructions = new Label();
             goBack = new Button();
+            rules = new Label();
             SuspendLayout();
             // 
             // titulo
             // 
             titulo.AutoSize = true;
             titulo.Font = new Font("Andalus", 30F, FontStyle.Bold, GraphicsUnit.Point, 178);
-            titulo.Location = new Point(281, 9);
+            titulo.Location = new Point(334, 9);
             titulo.Name = "titulo";
             titulo.Size = new Size(167, 61);
             titulo.TabIndex = 1;
@@ -41,34 +43,47 @@
             // instructions
             // 
             instructions.AutoSize = true;
-            instructions.Location = new Point(36, 96);
+            instructions.Font = new Font("Andalus", 15F, FontStyle.Bold, GraphicsUnit.Point, 178);
+            instructions.Location = new Point(12, 70);
             instructions.Name = "instructions";
-            instructions.Size = new Size(224, 15);
+            instructions.Size = new Size(800, 93);
             instructions.TabIndex = 2;
-            instructions.Text = "Cómo jugar al chinchón. Próximamente";
+            instructions.Text = resources.GetString("instructions.Text");
             // 
             // goBack
             // 
             goBack.Font = new Font("Andalus", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 178);
-            goBack.Location = new Point(673, 371);
+            goBack.Location = new Point(717, 851);
             goBack.Name = "goBack";
             goBack.Size = new Size(115, 58);
             goBack.TabIndex = 4;
             goBack.Text = "Volver";
             goBack.UseVisualStyleBackColor = true;
-            goBack.Click += this.goBack_Click;
+            goBack.Click += goBack_Click;
+            // 
+            // rules
+            // 
+            rules.AutoSize = true;
+            rules.Font = new Font("Andalus", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 178);
+            rules.Location = new Point(12, 173);
+            rules.Name = "rules";
+            rules.Size = new Size(651, 720);
+            rules.TabIndex = 5;
+            rules.Text = resources.GetString("rules.Text");
             // 
             // Tutorial
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 441);
+            ClientSize = new Size(844, 921);
+            Controls.Add(rules);
             Controls.Add(goBack);
             Controls.Add(instructions);
             Controls.Add(titulo);
             Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
             Name = "Tutorial";
-            Text = "TUTORIAL";
+            Text = "Tutorial";
+            Load += Tutorial_Load;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -78,5 +93,6 @@
         private Label titulo;
         private Label instructions;
         private Button goBack;
+        private Label rules;
     }
 }
--- Tutorial.cs
+++ Tutorial.cs
@@ -4,6 +4,16 @@
             InitializeComponent();
         }
 
+        // Redimensionar el formulario al cargarlo
+        private void Tutorial_Load(object sender, EventArgs e) {
+            // Tamaño
+            this.Size = new Size(860, 960);
+
+            // No se puede redimensionar
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+        }
+
         // Volver al menú principal
         private void goBack_Click(object sender, EventArgs e) {
             // Cerrar este formulario
